42895dd2c211f57d574d5d6640fe42e41436b84b
pablofretes <pablofretes.balzarelli@gmail.com>
2022-11-10 22:06:44 -0300

    delete logs

--- budget-backend/src/test-utils/test-database.providers.ts
+++ budget-backend/src/test-utils/test-database.providers.ts
@@ -5,26 +5,20 @@ import { Balance } from "../modules/balance/balance.entity";
 import { Movement } from "../modules/movements/movements.entity";
 
 export const setupDataSource = async () => {
-	console.log('here')
   const db = newDb({ autoCreateForeignKeyIndices: true });
-	console.log('here1')
 	
   db.public.registerFunction({
 		implementation: () => 'test',
     name: 'current_database',
   });
-
-	console.log('here3')
 	
   const dataSource: DataSource = await db.adapters.createTypeormDataSource({
 		type: 'postgres',
     entities: [User, Balance, Movement],
   });
-	console.log('here4')
+
   await dataSource.initialize();
-	console.log('here5')
   await dataSource.synchronize();
-	console.log('here6')
   
   return dataSource;
 };
